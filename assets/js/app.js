bc3a3cf64f85635aef847d027e676cef73674d63
Dean Slama Jr <deanslamajr@gmail.com>
2016-07-03 19:42:27 -0500

    removed lodash from app dependencies

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,18 @@
 'use strict';
 
-// Styles
+// Styles, webpack needs this entry point
 import '../css/app.css';
 
 // Add polyfills for full ES5 + ES6 environment with some new ECMAScript feature proposals(e.g. Array.prototype.includes())
 import "babel-polyfill";
 
 import angular from 'angular';
-import _       from 'lodash';
 
 // Registers as angular module
 import 'angular-animate';
 
 import main from './app-main';
 import config from './app-config';
-
-window._ = _;
 
 import services    from './services';
 import directives  from './directives';
