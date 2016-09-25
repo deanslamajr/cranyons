caa94e63bcc1390e98df2bc276dae262777fdd51
Dean Slama Jr <deanslamajr@gmail.com>
2016-09-25 15:32:43 -0500

    changed dev webpack sourcemap type
    added index checking in cranyonservice.discardCranyonDSFromCacheArray to avoid always removing an element from the array
    
    added setCranyonCtrlAsActive to encapsulate all of the tasks associated with the setting of a particular cranyon to active state
    
    continued adding unit tests for cranyon.service

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -2,5 +2,5 @@ const merge = require('webpack-merge');
 const nontest = require('./webpack.nontest.config');
 
 module.exports = merge.smart(nontest, {
-  devtool: 'inline-source-map',
+  devtool: 'cheap-eval-source-map',
 });
