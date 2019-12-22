bff66a960cf430d9a2e588c2bf59383012e0c6fc
Dean Slama <deanslamajr@gmail.com>
2019-12-22 10:06:04 -0700

    dockerize: added redirect to https

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express     = require('express');
 const path 	      = require('path');
 const bodyParser  = require('body-parser');
+const {redirectToHTTPS} = require('express-http-to-https');
+
 const router 	    = require('./routes');
 
 const app = express();
@@ -11,6 +13,8 @@ app.set('port', (process.env.PORT || 1337))
 
 // to serve local files - for dev only
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(redirectToHTTPS([/localhost:(\d{4})/]));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
