d30c0ff90cd4a67c4c6d76553cbd34914ee2894e
Dean Slama <deanslamajr@gmail.com>
2019-12-22 10:25:25 -0700

    dockerize: tried an alternative https redirect algorithm

--- app.js
+++ app.js
@@ -3,18 +3,29 @@
 const express     = require('express');
 const path 	      = require('path');
 const bodyParser  = require('body-parser');
-const {redirectToHTTPS} = require('express-http-to-https');
 
 const router 	    = require('./routes');
 
 const app = express();
 
+// if in production envs
+// only allow https connections
+if (process.env.NODE_ENV === 'production') {
+	app.enable('trust proxy');
+
+	app.use(function(req, res, next){
+		if (req.header('x-forwarded-proto') !== 'https') {
+			res.redirect('https://' + req.header('host') + req.url);
+		} else{
+			next();
+		}
+	})
+}
+
 app.set('port', (process.env.PORT || 1337))
 
 // to serve local files - for dev only
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(redirectToHTTPS([/localhost:(\d{4})/]));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
