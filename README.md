11e6fb8c51a2e3a74c69c92cf2c0b1f04b21e0f7
Dean Slama <deanslamajr@gmail.com>
2023-11-09 14:45:32 -0700

    deleted v1 assets

--- README.md
+++
@@ -1,6 +0,0 @@
-## Deploy to Heroku
-* `npm run deploy`
-* commit new `public/index.html`
-* `npm version major|minor|patch`
-* `git push origin master`
-* `git push heroku master`
--- config/webpack.dev.config.js
+++
@@ -1,6 +0,0 @@
-const merge = require('webpack-merge');
-const nontest = require('./webpack.nontest.config');
-
-module.exports = merge.smart(nontest, {
-  devtool: 'cheap-eval-source-map',
-});
