c9b5973c38a4a2cef34b150b22485b8da0d13646
Dean Slama <deanslamajr@gmail.com>
2023-11-09 12:55:22 -0700

    initialization of nextjs project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // for docker image builds, as described in official README: https://github.com/vercel/next.js/tree/canary/examples/with-docker
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
